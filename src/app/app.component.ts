cf1beebf15d4ec4858d32a5410cb6f26c9d41892
Dileep Kumar Damera <dileep.damera@forsys.taas.com>
2022-11-21 17:18:32 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Application';
+  selectedValue =  '';
+  myForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) {
+    
+  }
+
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      fName: 'Sammy',
+      lName: '',
+      email: '',
+      sex: ''
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {AccordionModule} from 'primeng/accordion';
+import {InputTextModule} from 'primeng/inputtext'
+import { FormsModule } from '@angular/forms';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,9 +18,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    InputTextModule,
+    RadioButtonModule,
+    ButtonModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
